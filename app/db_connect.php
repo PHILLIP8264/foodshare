1311d281abd08c41e6e49d14df49e456af502709
PHILLIP8264 <113760278+PHILLIP8264@users.noreply.github.com>
2024-11-07 23:30:36 +0200

    5

--- app/db_connect.php
+++ app/db_connect.php
@@ -1,14 +1,13 @@
 <?php
-$host = '/cloudsql/YOUR_PROJECT_ID:sanguine-fx-441010-h4:europe-west9:foodshareapp';
+$host = '/cloudsql/sanguine-fx-441010-h4:europe-west9:foodshareapp';
 $dbname = 'foodshare';
-$username = 'root';
-$password = '4/bPV6Aoz"NlzTo9';
+$username = 'root';  // Update this if the username is different
+$password = '4/bPV6Aoz"NlzTo9';  // Replace this with your actual password
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:unix_socket=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
-?>
